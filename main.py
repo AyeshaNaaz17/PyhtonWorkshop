96e6bd8efacf244e82ce623d18526b353ab6a435
AyeshaNaaz17 <ayeshanaazn17@gmail.com>
2022-11-13 07:48:02 +0530

    Python Workshop.

--- main.py
+++ main.py
@@ -38,10 +38,69 @@ def fact():
         print(fact1)
 
 ##6
+def reverse():
+    num5 = int(input())
+    n = 0
+    while (num5 > 0):
+        rem = num5 % 10
+        n = (n * 10) + rem
+        num5 = num5 // 10
+    print(n)
+
+##7
+def palindrome():
+    num6 = int(input())
+    x = num6
+    n = 0
+    while (num6 > 0):
+        rem = num6 % 10
+        n = (n * 10) + rem
+        num6 = num6 // 10
+    if (n == x):
+        print("Palindrome")
+    else:
+        print("Not Palindrome")
+
+##8
+def sumOfDigits():
+    n = 0
+    num7 = int(input())
+    y = num7
+    while (num7 > 0):
+        u = num7 % 10
+        n = n + u
+        num7 //= 10
+    print(n)
+
+##9
+def totalDigits():
+    num8 = int(input())
+    count = 0
+    while (num8 > 0):
+        num8 //= 10
+        count += 1
+    print(count)
+
+##10
+def zeroesAndOnes():
+    num9 = input()
+    x = ''
+    for i in num9:
+        if (i == '0'):
+            x += '1'
+        else:
+            x += '0'
+    print(x)
 
 
-#cube()
-#mul()
-#sumsquare()
-#readsum()
-fact()
+# cube()
+# mul()
+# sumsquare()
+# readsum()
+# fact()
+# reverse()
+# palindrome()
+# sumOfDigits()
+# totalDigits()
+# zeroesAndOnes()
+
